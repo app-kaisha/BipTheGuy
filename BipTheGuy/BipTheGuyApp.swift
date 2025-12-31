0b52cd999737cbb6ca1049c927ef0739d6e8807f
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-31 16:56:59 +0000

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,18 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by app-kaihatsusha on 31/12/2025.
+//  Copyright © 2025 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
